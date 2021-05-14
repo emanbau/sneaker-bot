3e3ea619af00146e340247f7c88adab2ac958e8d
Eman Bau <emanbautista410@gmail.com>
2021-05-14 17:18:02 -0400

    Size Selector & Add to Cart

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 
+// Variables
 const url = 'https://kith.com/collections/mens-footwear/products/cn171858c';
+const size = '9';
 
 const sneakerBot = () => {
     (async () => {
@@ -9,10 +11,13 @@ const sneakerBot = () => {
             const browser = await puppeteer.launch(
                 {
                     ignoreHTTPSErrors: true,
-                    headless: false
+                    headless: false,
+                    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+                    args: [ '--start-fullscreen' ]
                 }
             )
             const page = await browser.newPage();
+            await page.setViewport({ width: 1280, height: 720 });
             const navigationPromise = page.waitForNavigation();
             
             // Load URL
@@ -20,8 +25,18 @@ const sneakerBot = () => {
             // Wait until page is loaded
             page.waitForNavigation({ waitUntil: 'networkidle0' });
 
+            // Size Selector
+            await page.waitForSelector('.product__swatches > .product__swatches-container > .product__swatch-select');
+            await page.select('select#SingleOptionSelector-0', size);
+
+            // Add to Cart
+            await page.waitForSelector('.product__form > button.product-form__add-to-cart');
+            await page.click('.product__form > button.product-form__add-to-cart');
+
         } catch (e) {
             console.error(e);
         }
     })();
 }
+
+sneakerBot();
